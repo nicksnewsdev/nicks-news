301916be5b40d7a97206c44ba5ad265d35dc307f
NicksNews <nicksnewscat@gmail.com>
2026-08-29 23:29:45 +0200

    Add files via upload

---
+++ workers/main.ts
@@ -0,0 +1,43 @@
+import { getBlogById, getBlogIdFromUrl, getLatestBlogs } from "../functions/api/blogs/blogs";
+
+interface Env {
+	DB: D1Database;
+	ASSETS: Fetcher;
+}
+
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+
+		if (url.pathname === "/api/blogs/latest") {
+			const amount = Number(url.searchParams.get("amount") ?? 5);
+			const blogs = await getLatestBlogs(env.DB, amount);
+
+			return Response.json(blogs);
+		}
+
+		if (url.pathname === "/api/blogs/current") {
+			const path = url.searchParams.get("path");
+
+			if (!path) {
+				return Response.json(null, { status: 400 });
+			}
+
+			const id = getBlogIdFromUrl(path);
+
+			if (!id) {
+				return Response.json(null, { status: 400 });
+			}
+
+			const blog = await getBlogById(env.DB, id);
+
+			if (!blog) {
+				return Response.json(null, { status: 404 });
+			}
+
+			return Response.json(blog);
+		}
+
+		return env.ASSETS.fetch(request);
+	}
+} satisfies ExportedHandler<Env>;
